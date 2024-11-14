6232891d68bed745bd297c3c0c1313ddb5622422
e5sad <leafapplepie@gmail.com>
2024-11-14 16:38:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {
     updateMessageBlock,
 } from '../../../../script.js';
 import { extension_settings, getContext, renderExtensionTemplateAsync } from '../../../extensions.js';
-import { secret_state } from '../../../../public/scripts/secrets.js';
+import { secret_state } from '../../../../../secrets.js';
 
 // Define provider options
 const llmProviders = {
